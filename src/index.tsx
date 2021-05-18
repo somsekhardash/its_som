3522951ed25a15bd6229431a25b0631fb92bffcd
Somsekhar Dash <somsekhardash@gmail.com>
2021-05-18 14:11:47 +0530

    final commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/App";
+import { Helmet } from "react-helmet";
+import ReactMapboxFactory from "react-mapbox-gl/lib/map";
 
-ReactDOM.render(<App />, document.getElementById("page-top"));
+ReactDOM.render(
+  <React.Fragment>
+    <Helmet>
+      <meta name="description" content="Somsekhar Dash Personal Portfolio" />
+    </Helmet>
+    <App />
+  </React.Fragment>,
+  document.getElementById("page-top")
+);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
